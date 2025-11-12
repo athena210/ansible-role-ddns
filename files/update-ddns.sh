7cd1c724b1dc873484f6d7fa8eb8d308e9963a2b
athena210 <236075846+athena210@users.noreply.github.com>
2025-11-12 05:57:36 +0400

    ipv6 support

--- files/update-ddns.sh
+++ files/update-ddns.sh
@@ -7,6 +7,26 @@ alert() {
     logger -t "ddns" "$1"
 }
 
+
+open_url() {
+    # -4 or -6
+    # url
+    ipver="$1"
+    url="$2"
+
+    # echo ">>$ipver $url"
+    if command -v curl 1> /dev/null; then
+        echo curl "$ipver" --silent "$url" 1> /dev/null
+        return $?
+    elif command -v wget 1> /dev/null ; then
+        echo wget "$ipver" --quiet -O - "$url" 1> /dev/null
+        return $?
+    fi
+    alert "Neither curl nor wget is installed. DDNS update canceled"
+    return 1
+}
+
+
 parse_config() {
     config_file="$1"
 
@@ -17,29 +37,24 @@ parse_config() {
         return 0
     fi
 
-    sed -n 's/^[\t ]*DDNS_URL[\t ]*=[\t ]*\(.*\?\)[\t ]*$/\1/p' "$config_file" | while read -r DDNS_URL; do
-
-        # echo ">> $DDNS_URL"
-        ret=9
-        if command -v curl 1> /dev/null; then
-            curl --silent "$DDNS_URL" 1> /dev/null
-            ret=$?
-        elif command -v wget 1> /dev/null ; then
-            wget --quiet -O - "$DDNS_URL" 1> /dev/null
-            ret=$?
+    sed -n 's/^[\t ]*DDNS4_URL[\t ]*=[\t ]*\(.*\?\)[\t ]*$/\1/p' "$config_file" | while read -r DDNS_URL; do
+        if open_url "-4" "$DDNS_URL" ; then
+            alert "DNS ipv4 updated from $config_file"
         else
-            alert "Neither curl nor wget is installed. DDNS update canceled"
-            return 1
+            alert "DNS4 ipv4 update failed from $config_file"
         fi
+    done
 
-        if [ "$ret" -eq 0 ] ; then
-            alert "DNS updated from $config_file"
+    sed -n 's/^[\t ]*DDNS6_URL[\t ]*=[\t ]*\(.*\?\)[\t ]*$/\1/p' "$config_file" | while read -r DDNS_URL; do
+        if open_url "-6" "$DDNS_URL" ; then
+            alert "DNS ipv6 updated from $config_file"
         else
-            alert "DNS update failed from $config_file"
+            alert "DNS4 ipv6 update failed from $config_file"
         fi
     done
 }
 
-ls "$config_dir"/*.conf | while read -r fname; do
+
+ls -1 "$config_dir"/*.conf | while read -r fname; do
     parse_config "$fname"
 done
